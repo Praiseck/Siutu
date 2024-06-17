9c9829314d7313723c4052e2d2d69f0195e46b6e
Praiseck <manferduval15@gmail.com>
2024-06-17 13:34:15 -0500

    firts commit

---
+++ siutu-backend/src/main/java/com/siutu/siutu_backend/SiutuBackendApplication.java
@@ -0,0 +1,13 @@
+package com.siutu.siutu_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiutuBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiutuBackendApplication.class, args);
+	}
+
+}
